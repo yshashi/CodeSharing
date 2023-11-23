59efbdfc5520a8093670b0febca15079dfd6fe2d
Sashikumar <yshashi30@gmail.com>
2023-11-23 10:02:59 +0530

    added html serve

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
